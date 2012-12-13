5b6eaa40befee4ca8db4990d4fdef102a16ba8e3
HmmmQuestionMark <support@clashnia.com>
2012-12-13 14:59:14 -0500

    A few small fixes to the updater script.

--- src/com/clashnia/ClashniaUpdate/DemigodsUpdate.java
+++ src/com/clashnia/ClashniaUpdate/DemigodsUpdate.java
@@ -9,9 +9,9 @@ import java.io.OutputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginDescriptionFile;
 
 import com.WildAmazing.marinating.Demigods.DUtil;
 import com.WildAmazing.marinating.Demigods.Demigods;
@@ -19,8 +19,6 @@ import com.WildAmazing.marinating.Demigods.Demigods;
 public class DemigodsUpdate
 {
 	static Demigods plugin;
-	
-	static Logger log = Logger.getLogger("Minecraft");
 	
 	/*
 	 *  (String)OLD_DOWNLOAD_LINK : The download link for what should be this exact jar, or the last stable jar if this is a development build.
@@ -30,6 +28,10 @@ public class DemigodsUpdate
 	
 	public static boolean shouldUpdate()
 	{
+		PluginDescriptionFile pdf = DUtil.getPlugin().getDescription();	  	
+		String currentVersion = pdf.getVersion();	  	  	
+		if (currentVersion.startsWith("d")) return false; // development versions shouldn't downgrade
+		
 		if (checker.updateNeeded()){
 			DUtil.consoleMSG("info","A new version is available: " + checker.getVersion());
 			return true;
@@ -50,7 +52,7 @@ public class DemigodsUpdate
 			int bytesTransferred = 0;
 			String downloadLink = getDownloadLink();
 
-			log.info("[Demigods] Attempting to update to latest version...");
+			DUtil.consoleMSG("info","Attempting to update to latest version...");
 			
 			// Set latest build URL
 			URL plugin = new URL(downloadLink);
@@ -62,7 +64,7 @@ public class DemigodsUpdate
 
             // Create new .jar file and add it to plugins directory
             File pluginUpdate = new File("plugins" + File.separator + "Demigods.jar");
-			log.info("[Demigods] File has been written to: " + pluginUpdate.getCanonicalPath());
+            DUtil.consoleMSG("info","File has been written to: " + pluginUpdate.getCanonicalPath());
 			
 			InputStream is = pluginCon.getInputStream();
 			OutputStream os = new FileOutputStream(pluginUpdate);
@@ -79,7 +81,7 @@ public class DemigodsUpdate
 					
 					if(percentTransferred != 100)
 					{
-						log.info("[Demigods] " + percentTransferred + "%");
+						DUtil.consoleMSG("info", percentTransferred + "%");
 					}
 				}
 			}
@@ -89,20 +91,20 @@ public class DemigodsUpdate
 			os.close();
 			
 			// Update complete! Reload the server now
-			log.info("[Demigods] Download complete! Reloading server...");
+			DUtil.consoleMSG("info","Download complete! Reloading server...");
 			Bukkit.getServer().reload();
 		}
 		catch (MalformedURLException ex)
 		{
-			log.warning("[Demigods] Error accessing URL: " + ex);
+			DUtil.consoleMSG("warning", "Error accessing URL: " + ex);
 		}
 		catch (FileNotFoundException ex)
 		{
-			log.warning("[Demigods] Error accessing URL: " + ex);
+			DUtil.consoleMSG("warning", "Error accessing URL: " + ex);
 		}
 		catch (IOException ex)
 		{
-			log.warning("[Demigods] Error downloading file: " + ex);
+			DUtil.consoleMSG("warning", "Error downloading file: " + ex);
 		}
 	}
 	
